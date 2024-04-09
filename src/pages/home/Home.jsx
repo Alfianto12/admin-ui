c4d6623558e6b62d5ad6789f10dce6b112f765ab
Alfianto12 <muhammadalfianto.25@gmail.com>
2024-04-09 18:12:18 +0700

    09/04/2024

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
+
 
 const Home = () => {
   return (
@@ -14,7 +16,11 @@ const Home = () => {
           <Widget type="user"/>
           <Widget type="order"/>
           <Widget type="earning"/>
-          <Widget type="range"/>
+          <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Chart type="order" aspect={2 / 1} />
+          <Chart type="earning" aspect={2 / 1} />
         </div>
       </div>
     </div>
