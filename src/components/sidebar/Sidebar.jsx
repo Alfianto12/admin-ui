b74b80b8728b0809c8055e25eea7d7de4a6c2fe9
Alfianto12 <muhammadalfianto.25@gmail.com>
2024-04-09 09:39:38 +0700

    09/04/2024

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+	<div className="top">
+	  <div className="logo">Store</div>
+	</div>
+	<div className="center">
+	  <ul>
+	    <li>
+	      <span>list</span>
+	    </li>
+	    <li>
+	      <span>list</span>
+	    </li>
+	    <li>
+	      <span>list</span>
+	    </li>
+	  </ul>
+	</div>
+	<div className="bottom">Color options</div>
+</div>
+    
+  );
+};
+
+export default Sidebar;
