7f0203d307d8b851548c43f1b836db7c9f34fab3
Alfianto12 <muhammadalfianto.25@gmail.com>
2024-05-05 19:30:12 +0700

    ndang

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,8 @@
 import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/datatable/Datatable";
 
 const Single = () => {
   return (
@@ -9,10 +11,50 @@ const Single = () => {
       <div className="singleContainer">
         <Navbar />
         <div className="top">
-          <div className="left">top left</div>
-          <div className="right">top right</div>
+
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.unsplash.com/photo-1700553792546-3fbfc6b6613e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+1 2345 67 89</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Elton St. 234 Garden Yd. NewYork
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="right">
+            <Chart aspect={3 / 1} type="order" />
+          </div>
+                    
         </div>
-        <div className="bottom">bottom</div>
+
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
