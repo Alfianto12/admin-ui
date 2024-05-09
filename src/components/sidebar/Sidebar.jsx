92c8f6a9e39d119932335d160d142294b8411344
Alfianto12 <muhammadalfianto.25@gmail.com>
2024-05-09 08:39:15 +0700

    uji ualng 5

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,64 +1,62 @@
 import "./sidebar.scss";
 import { Link } from "react-router-dom";
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import Inventory2Icon from '@mui/icons-material/Inventory2';
-import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
-
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
     <div className="top">
-      <Link to="/">
+        <Link to="/">
         <span className="logo">Store</span>
-      </Link>
+        </Link>
     </div>
     <hr />
     <div className="center">
-      <ul>
+        <ul>
         <p className="title">MAIN</p>
         <li>
-          <DashboardIcon className="icon"/>
-          <span>Dashboard</span>
+            <DashboardIcon className="icon"/>
+            <span>Dashboard</span>
         </li>
         <p className="title">LIST</p>
         <Link to="/users">
-          <li>
-            <AccountBoxIcon className="icon"/>
+            <li>
+            <PersonOutlineIcon className="icon"/>
             <span>Users</span>
-          </li>
+            </li>
         </Link>
         <Link to="/products">
-          <li>
-            <Inventory2Icon className="icon"/>
+            <li>
+            <StorefrontIcon className="icon"/>
             <span>Products</span>
-          </li>
+            </li>
         </Link>
         <li>
-          <ShoppingCartCheckoutIcon className="icon"/>
-          <span>Orders</span>
+            <LocalGroceryStoreIcon className="icon"/>
+            <span>Orders</span>
         </li>
         <p className="title">USER</p>
         <li>
-          <InsertEmoticonIcon className="icon"/>
-          <span>Profile</span>
+            <AccountCircleOutlinedIcon className="icon"/>
+            <span>Profile</span>
         </li>
         <li>
-          <LogoutIcon className="icon"/>
-          <span>Logout</span>
+            <LogoutIcon className="icon"/>
+            <span>Logout</span>
         </li>
-      </ul>
+        </ul>
     </div>
     <div className="bottom">
-      <div className="colorOption"></div>
-      <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        </div>
     </div>
-  </div>
-    
-  )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
