d5a49858db0ca9e5cbc9ca5d7e154d618ac9177c
Alfianto12 <muhammadalfianto.25@gmail.com>
2024-06-05 05:11:06 +0700

    yok

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,18 @@
 import "./login.scss"; 
-import { useState } from "react";
+import { useContext,useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => { 
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navitage = useNavigate()
+
+  const {dispatch} = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -14,8 +20,9 @@ const Login = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user; 
-        console.log(user);
+        const user = userCredential.user;
+        dispatch({type:"LOGIN", payload:user});
+        navitage("/");
       })
       .catch((error) => {
         setError(true);
