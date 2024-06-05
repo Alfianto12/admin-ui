0946749ea42a22806ceb488577cc454b284a78c5
Alfianto12 <muhammadalfianto.25@gmail.com>
2024-06-05 12:07:40 +0700

    firebaseku

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,18 @@
-import "./login.scss";
+import "./login.scss"; 
 import { useContext, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext";
 
-const Login = () => {
+const Login = () => { 
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate()
-  const { dispatch } = useContext(AuthContext);
+
+  const {dispatch} = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -19,8 +20,8 @@ const Login = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user;
-        dispatch({ type: "LOGIN", payload: user });
+        const user = userCredential.user; 
+        dispatch({type:"LOGIN", payload:user});
         navigate("/");
       })
       .catch((error) => {
@@ -33,12 +34,12 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <input
           type="email"
-          placeholder="email"
+          placeholder="email" 
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
-          placeholder="password"
+          placeholder="password" 
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Login</button>
